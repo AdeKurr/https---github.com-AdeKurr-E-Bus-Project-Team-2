204603882928a9370e9a73673955e1820c6f0da4
Ade Kurniawan <90836316+AdeKurr@users.noreply.github.com>
2021-12-06 13:13:02 +0700

    Delete src directory
    Rusak, mau upload lokal yang masih jalan headernya

--- src/main/java/com/project/ebus/repository/keberangkatanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.ebus.repository;
-
-import com.project.ebus.model.Keberangkatan;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface keberangkatanRepository extends JpaRepository<Keberangkatan, Long>{
-    
-}
