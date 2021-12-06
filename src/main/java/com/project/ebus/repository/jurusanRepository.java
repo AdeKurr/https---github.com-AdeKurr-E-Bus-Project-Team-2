5940122e00f3bbfb2a975bff5c7534354249c7ce
Ade Kurniawan <90836316+AdeKurr@users.noreply.github.com>
2021-12-06 13:37:20 +0700

    Delete src/main/java/com/project/ebus directory

--- src/main/java/com/project/ebus/repository/jurusanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.ebus.repository;
-
-import com.project.ebus.model.Jurusan;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface jurusanRepository extends JpaRepository<Jurusan, Long> {
-
-}
--- src/main/java/com/project/ebus/repository/perusahaanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.ebus.repository;
-
-import com.project.ebus.model.Perusahaan;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface perusahaanRepository extends JpaRepository<Perusahaan, Long> {
-
-}
