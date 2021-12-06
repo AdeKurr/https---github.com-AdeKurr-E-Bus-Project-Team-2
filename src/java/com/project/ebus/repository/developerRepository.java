913cf1e1bde7ac11b66aba55f0a18ef3741178df
Ade Kurniawan <90836316+AdeKurr@users.noreply.github.com>
2021-12-06 13:44:10 +0700

    Delete src directory

--- src/java/com/project/ebus/repository/developerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.ebus.repository;
-
-import com.project.ebus.model.Developers;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface developerRepository extends JpaRepository<Developers, Long> {
-
-}
